f671df29681ea532b3cacd677c67219670548ad8
Sri Revanth Reddipalli <reddipallisrirevanth@gmail.com>
2025-02-21 17:42:02 +0530

    Added SQL script for database setup

---
+++ DatabaseScripts/TaskManagementDB.sql
@@ -0,0 +1,43 @@
+﻿CREATE DATABASE TaskManagementDB;
+
+USE TaskManagementDB;
+
+-- Create Tenants Table
+CREATE TABLE Tenants (
+    tenantId UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+    organizationName VARCHAR(255) NOT NULL,
+    createdDate DATETIME DEFAULT GETDATE()
+);
+
+-- Create Table users
+CREATE TABLE Users (
+    userId UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+    name VARCHAR(255),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    passwordHash VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    tenantId UNIQUEIDENTIFIER,
+    FOREIGN KEY (tenantId) REFERENCES Tenants(tenantId)
+);
+
+-- Create Projects Table
+CREATE TABLE Projects (
+    projectId UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+    name VARCHAR(255),
+    description TEXT,
+    tenantId UNIQUEIDENTIFIER,
+    FOREIGN KEY (tenantId) REFERENCES Tenants(tenantId)
+);
+
+-- Create Tasks Table
+CREATE TABLE Tasks (
+    taskId UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+    title VARCHAR(255),
+    status VARCHAR(50),
+    assignedTo UNIQUEIDENTIFIER,
+    projectId UNIQUEIDENTIFIER,
+    tenantId UNIQUEIDENTIFIER,
+    FOREIGN KEY (projectId) REFERENCES Projects(projectId),
+    FOREIGN KEY (tenantId) REFERENCES Tenants(tenantId),
+    FOREIGN KEY (assignedTo) REFERENCES Users(userId)
+);
